9e3b344a033b3c02f8353d474f693ddb11e557aa
dabhisharad13 <dabhisharad13@gmail.com>
2023-04-23 16:09:44 -0700

    Added mongoose db connection configuration

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
+//imports
+//jslint-ignore-line
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
-// importing the config  file
+// importing the config file
 dotenv.config({ path: "config.env" });
-
-//setting port
-const PORT = process.env.PORT || 8080;
+dotenv.config({ path: "db.env" });
 
 //log request
 app.use(morgan("tiny"));
+
+//setting port
+const PORT = process.env.PORT || 8080;
+const DATABASE = process.env.DATABASE;
+
+//database connection
+mongoose
+  .connect(DATABASE)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //routes
 app.get("/", (req, res) => {
